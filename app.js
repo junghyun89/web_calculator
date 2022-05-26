63262d68a8ef0c09a5465842a18aaeb249978caf
jung89 <104253822+junghyun89@users.noreply.github.com>
2022-05-26 11:30:28 +0900

    YAY

---
+++ app.js
@@ -0,0 +1,103 @@
+"use strict";
+
+let numOne = "";
+let operator = "";
+let numTwo = "";
+const $operator = document.querySelector("#operator");
+const $result = document.querySelector("#result");
+const onClickNumber = (event) => {
+  if (numTwo) {
+    switch (operator) {
+      case "+":
+        $result.value = parseInt(numOne) + parseInt(numTwo);
+        break;
+      case "-":
+        $result.value = numOne - numTwo;
+        break;
+      case "*":
+        $result.value = numOne * numTwo;
+        break;
+      case "/":
+        $result.value = numOne / numTwo;
+        break;
+      default:
+        break;
+    }
+    numOne = $result.value;
+    numTwo = "";
+  }
+  const number = event.target.textContent;
+  if (!operator) {
+    numOne += number;
+    $result.value += number;
+    return;
+  }
+  if (!numTwo) {
+    $result.value = "";
+  }
+  numTwo += number;
+  $result.value += number;
+};
+document.querySelector("#num-0").addEventListener("click", onClickNumber);
+document.querySelector("#num-1").addEventListener("click", onClickNumber);
+document.querySelector("#num-2").addEventListener("click", onClickNumber);
+document.querySelector("#num-3").addEventListener("click", onClickNumber);
+document.querySelector("#num-4").addEventListener("click", onClickNumber);
+document.querySelector("#num-5").addEventListener("click", onClickNumber);
+document.querySelector("#num-6").addEventListener("click", onClickNumber);
+document.querySelector("#num-7").addEventListener("click", onClickNumber);
+document.querySelector("#num-8").addEventListener("click", onClickNumber);
+document.querySelector("#num-9").addEventListener("click", onClickNumber);
+
+const onClickOperator = (op) => () => {
+  if (numOne) {
+    operator = op;
+    $operator.value = op;
+  } else {
+    alert("숫자를 먼저 입력하세요.");
+  }
+};
+
+document.querySelector("#plus").addEventListener("click", onClickOperator("+"));
+document
+  .querySelector("#minus")
+  .addEventListener("click", onClickOperator("-"));
+document
+  .querySelector("#divide")
+  .addEventListener("click", onClickOperator("/"));
+document
+  .querySelector("#multiply")
+  .addEventListener("click", onClickOperator("*"));
+document.querySelector("#clear").addEventListener("click", () => {
+  numOne = "";
+  operator = "";
+  numTwo = "";
+  $operator.value = "";
+  $result.value = "";
+});
+document.querySelector("#calculate").addEventListener("click", () => {
+  if (numTwo) {
+    switch (operator) {
+      case "+":
+        $result.value = parseInt(numOne) + parseInt(numTwo);
+        break;
+      case "-":
+        $result.value = numOne - numTwo;
+        break;
+      case "*":
+        $result.value = numOne * numTwo;
+        break;
+      case "/":
+        $result.value = numOne / numTwo;
+        break;
+      default:
+        break;
+    }
+    $operator.value = "";
+    numOne = $result.value;
+    operator = "";
+    numTwo = "";
+  } else {
+    alert("숫자를 먼저 입력하세요.");
+  }
+});
